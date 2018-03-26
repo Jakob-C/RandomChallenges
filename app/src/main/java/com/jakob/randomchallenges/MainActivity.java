6f5e0bd7ac0fd27d0666b95c7355f22f85ef62d4
Aaron <aaron.wtal@googlemail.com>
2018-03-26 15:49:25 +0200

    hhh

--- app/src/main/java/com/jakob/randomchallenges/MainActivity.java
+++ app/src/main/java/com/jakob/randomchallenges/MainActivity.java
@@ -1,12 +1,21 @@
 package com.jakob.randomchallenges;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Vibrator;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +23,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -24,19 +39,27 @@ public class MainActivity extends AppCompatActivity {
     Button easy, hard, expert;
     TextView playbutton;
     int angle;
-    ImageView pointer,wheel;
+    ImageView pointer, greenwheel, orangewheel, redwheel;
+    DrawerLayout mDrawerLayout;
+    NavigationView mNavigationView;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mNavigationView = (NavigationView) findViewById(R.id.shitstuff);
 
         easy=(Button)findViewById(R.id.easy);
         hard=(Button)findViewById(R.id.hard);
         expert=(Button)findViewById(R.id.expert);
 
-        wheel=(ImageView)findViewById(R.id.wheel);
+        greenwheel=(ImageView)findViewById(R.id.greenwheel);
+        orangewheel=(ImageView)findViewById(R.id.orangewheel);
+        redwheel=(ImageView)findViewById(R.id.redwheel);
 
         playbutton=(TextView)findViewById(R.id.playbutton);
         Typeface burbank = Typeface.createFromAsset(getApplication().getAssets(), "burbank.otf");
@@ -81,28 +104,65 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        mNavigationView.setItemIconTintList(null);
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                mDrawerLayout.closeDrawers();
+
+
+
+
+                return false;
+            }
+
+        });
+
+        /**
+         * Setup Drawer Toggle of the Toolbar
+         */
+
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name,
+                R.string.app_name);
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mDrawerToggle.syncState();
+
+
     }
 
     public void easy (View v){
-        wheel.setImageResource(R.drawable.greenwheel);
+        greenwheel.setVisibility(View.VISIBLE);
+        orangewheel.setVisibility(View.INVISIBLE);
+        redwheel.setVisibility(View.INVISIBLE);
         easy.setAlpha(1f);
         hard.setAlpha(0.2f);
         expert.setAlpha(0.2f);
     }
 
     public void hard (View v){
-        wheel.setImageResource(R.drawable.orangewheel);
+        orangewheel.setVisibility(View.VISIBLE);
+        greenwheel.setVisibility(View.INVISIBLE);
+        redwheel.setVisibility(View.INVISIBLE);
         easy.setAlpha(0.2f);
         hard.setAlpha(1f);
         expert.setAlpha(0.2f);
     }
 
     public void expert (View v){
-        wheel.setImageResource(R.drawable.redwheel);
+        redwheel.setVisibility(View.VISIBLE);
+        greenwheel.setVisibility(View.INVISIBLE);
+        orangewheel.setVisibility(View.INVISIBLE);
         easy.setAlpha(0.2f);
         hard.setAlpha(0.2f);
         expert.setAlpha(1f);
     }
+
 
 
 }
